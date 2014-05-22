3ddbfb882a34fba64da43eac93bda71640219470
Sergey Shnaidman <sshnaidm@cisco.com>
2014-05-22 12:36:47 +0400

    Added special tempest scripts for initialization

---
+++ tools/tempest-scripts/tempest_configurator.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#cd /root/
+#source /root/openrc
+#wget http://download.cirros-cloud.net/0.3.2/cirros-0.3.2-x86_64-disk.img
+#glance image-create --name=cirros-0.3-x86_64 --is-public=true --container-format=bare --disk-format=qcow2 < cirros-0.3.2-x86_64-disk.img
+echo "Reinitialize the dir"
+rm -rf prepare_for_tempest_dir
+mkdir -p prepare_for_tempest_dir
+cd prepare_for_tempest_dir/
+echo "downloading cirros-0.3.1-x86_64-disk.img ...."
+wget -nv http://172.29.173.233/cirros-0.3.1-x86_64-disk.img
+#wget -nv http://download.cirros-cloud.net/0.3.1/cirros-0.3.1-x86_64-disk.img
+glance image-create --name=cirros-0.3-x86_64 --is-public=true --container-format=bare --disk-format=qcow2 < ./cirros-0.3.1-x86_64-disk.img
+#wget http://uec-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img
+#glance image-create --name=trusty-server --is-public=true --container-format=bare --disk-format=qcow2 < ./trusty-server-cloudimg-amd64-disk1.img
+echo "downloading precise-server-cloudimg-amd64-disk1.img ...."
+#wget -nv http://uec-images.ubuntu.com/server/precise/current/precise-server-cloudimg-amd64-disk1.img
+#wget -nv http://172.29.173.233/precise-server-cloudimg-amd64-disk1.img
+glance image-create --name=precise-server  --is-public=true --container-format=bare --disk-format=qcow2 < ./cirros-0.3.1-x86_64-disk.img
+keystone tenant-create --name demo
+kid1=$(keystone tenant-list | grep " demo " | awk {'print $2'})
+keystone user-create --name=demo --pass=secret --tenant-id=$kid1 --email=demo@domain1.com
+keystone tenant-create --name alt_demo
+kid2=$(keystone tenant-list | grep " alt_demo " | awk {'print $2'})
+keystone user-create --name=alt_demo --pass=secret --tenant-id=$kid2  --email=alt_demo@domain1.com
+keystone tenant-create --name openstack
+kid3=$(keystone tenant-list | grep " openstack " | awk {'print $2'})
+keystone user-create --name=admin --pass=Cisco123 --tenant-id=$kid3 --email=admin@domain1.com
+uid1=$(keystone user-list | grep " admin " | awk {'print $2'})
+rid1=$(keystone role-list | grep " admin " |  awk {'print $2'})
+keystone user-role-add --tenant-id=$kid3 --user-id=$uid1 --role-id=$rid1
+keystone user-role-add --tenant-id=$kid1 --user-id=$uid1 --role-id=$rid1
+neutron net-create public --router:external=True
+neutron subnet-create --allocation-pool start=192.168.1.230,end=192.168.1.250 public 192.168.1.224/27
+neutron net-create net10 --shared
+neutron subnet-create net10 10.10.10.0/24 --dns_nameservers list=true 8.8.8.8 8.8.4.4
+neutron router-create router1
+sid=$(neutron subnet-list | grep "10.10.10.0/24" |  awk {'print $2'})
+neutron router-interface-add router1 $sid
+neutron router-gateway-set router1
+nid=$(neutron net-list | grep " public " |  awk {'print $2'}) 
+neutron router-gateway-set router1 $nid
+
+cd /tmp
+wget -nv http://download.cirros-cloud.net/0.3.1/cirros-0.3.1-x86_64-uec.tar.gz
+sudo apt-get install -y python-pip
+sudo pip install testtools
+sudo pip install testresources
+
---
+++ tools/tempest-scripts/tempest_unconfig.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#cd /root/
+source ./openrc
+rm -rf prepare_for_tempest_dir
+echo "deleting cirros-0.3.1-x86_64-disk.img ...."
+rm ./cirros-*img
+echo "deleting all glance images ....."
+glance image-list | grep qcow2 | awk {'print $2'} | xargs glance image-delete
+echo "deleting precise-server-cloudimg-amd64-disk1.img ...."
+rm ./precise-server-cloudimg-amd64-disk1.img
+echo "deleting all tenants with demo ...."
+#keystone tenant-list | grep "demo" | awk {'print $2'} | xargs keystone tenant-delete
+keystone tenant-delete demo
+keystone tenant-delete alt_demo
+echo "deleting all users demo ...."
+keystone user-delete demo
+keystone user-delete alt_demo
+echo "clearing gateway from routers ...."
+neutron router-list | grep " router1 " | awk {'print $2'} | xargs neutron router-gateway-clear
+pslist=$(neutron port-list | grep subnet | awk {'print $2'})
+routerlist=$(neutron router-list | grep " router1 " | awk {'print $2'})
+for i in $routerlist; do 
+    for j in $pslist; do
+        echo "deleting interface $j in router $i ...."
+        neutron router-interface-delete $i port=$j;
+        neutron port-delete $j; 
+    done
+done
+echo "deleting routers ...."
+for i in $routerlist; do neutron router-delete $i; done
+echo "deleting networks ...."
+for i in $(neutron net-list | grep -E "public|net10" |  awk {'print $2'}); do neutron net-delete $i; done
+echo "deleting subnets ...."
+for i in $(neutron subnet-list | grep start | awk {'print $2'}); do neutron subnet-delete $i; done
+echo "deleting cirros from tmp ...."
+cd /tmp
+rm -rf cirros-0.3.1-x86_64-uec*
