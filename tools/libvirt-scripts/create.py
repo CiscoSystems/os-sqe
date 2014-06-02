322e6a13582889d3812bba0c45035db4d736db57
Sergey Shnaidman <sshnaidm@cisco.com>
2014-06-02 15:45:07 +0400

    test1

--- tools/libvirt-scripts/create.py
+++ tools/libvirt-scripts/create.py
@@ -7,9 +7,11 @@ import os
 import logging
 import subprocess
 
+from tempfile import mkstemp
 from libvirt import libvirtError
 
 __author__ = 'sshnaidm'
+
 
 def rand_mac():
     mac = [0x52, 0x54, 0x00,
@@ -19,10 +21,8 @@ def rand_mac():
     return ':'.join(["%02x" % x for x in mac])
 
 ## TODO: remove GLOBALS
-IMG_PATH = IMG_PATH_LOCAL=os.path.join(os.environ["HOME"], "hdd", "tmpdir","tmp", "imgs")
 name_woext = "trusty-server-cloudimg-amd64-disk1"
-ubuntu_img_path_local=os.path.join(os.environ["HOME"], "hdd", "tmpdir", name_woext + ".img")
-EXTERNAL_NET = "public"
+EXTERNAL_NET = "external"
 DOMAIN_NAME = "domain.name"
 
 
@@ -30,6 +30,7 @@ def handler(ctxt, err):
     global errno
     errno = err
 libvirt.registerErrorHandler(handler, 'context')
+
 
 def run_cmd(cmd):
     """
@@ -43,8 +44,10 @@ def run_cmd(cmd):
     print "\n".join(proc.stdout.readlines()), "\n".join(proc.stderr.readlines())
     return ret, proc.stdout.readlines(), proc.stderr.readlines()
 
+
 def vm_create(conn, xml):
     return conn.defineXML(xml)
+
 
 def net_create(conn, xml):
     net = conn.networkDefineXML(xml)
@@ -52,10 +55,12 @@ def net_create(conn, xml):
     net.setAutostart(True)
     return net
 
+
 def net_undefine(conn, net_name):
     net = conn.networkLookupByName(net_name)
     net.destroy()
     net.undefine()
+
 
 def pool_delete(conn, name):
     pool = conn.storagePoolLookupByName(name)
@@ -65,11 +70,13 @@ def pool_delete(conn, name):
         print "Pool wans't active, just undefine it"
     pool.undefine()
 
+
 def pool_create(conn, xml):
     pool = conn.storagePoolDefineXML(xml)
     pool.create()
     pool.setAutostart(True)
     return pool
+
 
 def pool_found(conn, name):
     try:
@@ -78,42 +85,47 @@ def pool_found(conn, name):
     except libvirtError:
         return False
 
+
 def convert_image(path, new_path):
     ret, ignore, stderr = run_cmd(["qemu-img", "convert", "-O",
-            "qcow2", path, new_path])
+                                   "qcow2", path, new_path])
     print "\n".join(ignore), "\n".join(stderr)
     if ret == 127:
         ret, ignore, stderr = run_cmd(["kvm-img", "convert", "-O",
-            "qcow2", path, new_path])
+                                       "qcow2", path, new_path])
     if ret != 0:
         raise RuntimeError("Disk conversion failed with "
-            "exit status %d: %s" % (ret, "".join(stderr)))
+                           "exit status %d: %s" % (ret, "".join(stderr)))
     #if len(stderr):
     #    print >> sys.stderr, stderr
 
-def main_disk_create(name, size, config, boot_type="net", user_seed_yaml=None, img_uncomp_path=None):
-    path = os.path.join(IMG_PATH, name + ".qcow2")
+
+def main_disk_create(name, size, config, img_path, boot_type="net", user_seed_yaml=None, img_uncomp_path=None):
+    path = os.path.join(img_path, name + ".qcow2")
     size_bytes = size*1024*1024*1024
     if boot_type == "net":
-        vol_xml=config["params"]["vol"]["xml"].format(name=name, size=size_bytes, path=path)
-        with open("/tmp/tmp_vol.xml", "w") as f:
+        vol_xml = config["params"]["vol"]["xml"].format(name=name, size=size_bytes, path=path)
+        _, tmp_file = mkstemp(prefix="disk_vol_")
+        with open(tmp_file, "w") as f:
             f.write(vol_xml)
-        run_cmd(["virsh", "vol-create",  "--pool", "default", "--file" ,"/tmp/tmp_vol.xml"])
+        run_cmd(["virsh", "vol-create",  "--pool", "default", "--file", tmp_file])
         return config["params"]["vol"]["virt_disk"].format(output_file=path)
     elif boot_type == "cloudimg":
-        seed_disk=os.path.join(IMG_PATH, name + "-seed.qcow2")
+        seed_disk = os.path.join(img_path, name + "-seed.qcow2")
         #img_uncomp_path=os.path.join(IMG_PATH, "lab-backing.qcow2")
-        user_seed_yaml_path = os.path.join("/tmp/", "tmp-yaml")
-        with open(user_seed_yaml_path, "w") as f:
+        _, tmp_file = mkstemp(prefix="useryaml_")
+        with open(tmp_file, "w") as f:
             f.write("#cloud-config\n" + user_seed_yaml)
         run_cmd(["qemu-img", "create", "-f", "qcow2", "-b", img_uncomp_path, path, "%sG" % size])
-        run_cmd(["cloud-localds", seed_disk, user_seed_yaml_path])
+        run_cmd(["cloud-localds", seed_disk, tmp_file])
         return config["params"]["vol"]["cloudimg_disk"].format(output_file=path, seed_disk=seed_disk)
+
 
 def create_vm(conn, xml):
     vm = conn.defineXML(xml)
     vm.create()
     return vm
+
 
 def delete_vm(conn, name):
     try:
@@ -128,9 +140,11 @@ def delete_vm(conn, name):
     vm.undefine()
     print "Domain {name} deleted".format(name=name)
 
-def remove_all_imgs():
-    for i in os.listdir(IMG_PATH):
-        os.remove(os.path.join(IMG_PATH, i))
+
+def remove_all_imgs(img_path, lab_id):
+    if os.path.exists(img_path):
+        for i in [j for j in os.listdir(img_path) if lab_id in j]:
+            os.remove(os.path.join(img_path, i))
 
 
 def create_seed_yaml(config, box=None):
@@ -144,6 +158,7 @@ def create_seed_yaml(config, box=None):
     user_seed_yaml['write_files'][0]['content'] = "\n".join(config['params']['id_rsa_pub'])
     return yaml.dump(user_seed_yaml)
 
+
 def create_host_seed_yaml(config, box, btype):
     user_seed_yaml_string = config['params'][btype]['user-yaml']
     user_seed_yaml = yaml.load(user_seed_yaml_string)
@@ -154,14 +169,15 @@ def create_host_seed_yaml(config, box, btype):
     networks = config['params']['networks']
     ints = []
     last_octet = box['ip'].split(".")[-1]
-    sorted_nets = sorted(networks.keys(), key=lambda x: int(networks[x]["net-ip"].split(".")[2]))
+    #sorted_nets = sorted(networks.keys(), key=lambda x: int(networks[x]["net-ip"].split(".")[2]))
+    sorted_nets = networks
     for k, net in enumerate(sorted_nets[1:]):
-        if net != EXTERNAL_NET:
+        if net['role'] != EXTERNAL_NET:
             ints.append([
                 config['params']['static_interface_template'].format(
                     int_name="eth"+str(k+1),
-                    net_ip=networks[net]['net-ip'],
-                    int_ip=networks[net]['net-ip'] + "." + last_octet,
+                    net_ip=net['net-ip'],
+                    int_ip=net['net-ip'] + "." + last_octet,
                     dns="8.8.8.8"),
                 "/etc/network/interfaces.d/eth{int_num}.cfg".format(int_num=k+1),
                 "eth{int_num}".format(int_num=k+1)
@@ -174,22 +190,22 @@ def create_host_seed_yaml(config, box, btype):
                 "eth{int_num}".format(int_num=k+1)
             ])
     for interface in ints:
-        user_seed_yaml['write_files'].append({
-                "content": interface[0],
-                "path": interface[1],
+        user_seed_yaml["write_files"].append({
+            "content": interface[0],
+            "path": interface[1],
             })
     hosts_file = config['params']['hosts_template'].format(
         server_name=box["hostname"],
         domain_name=DOMAIN_NAME,
     )
-    user_seed_yaml['write_files'].append({
-                "content": hosts_file,
-                "path": "/etc/hosts",
-                })
-    user_seed_yaml['write_files'].append({
-                "content": box["hostname"],
-                "path": "/etc/hostname",
-                })
+    user_seed_yaml["write_files"].append({
+        "content": hosts_file,
+        "path": "/etc/hosts",
+        })
+    user_seed_yaml["write_files"].append({
+        "content": box["hostname"],
+        "path": "/etc/hostname",
+        })
     full_cmd = []
     for cmd in user_seed_yaml['runcmd']:
         if "hostname" in cmd:
@@ -203,13 +219,12 @@ def create_host_seed_yaml(config, box, btype):
     user_seed_yaml['runcmd'] = full_cmd
     return yaml.dump(user_seed_yaml)
 
+
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', action='store', dest='user',
                         help='User to run the script with')
-    parser.add_argument('-p', action='store', dest='password',
-                        help='Password for user and sudo')
     parser.add_argument('-a', action='store', dest='host', default=None,
                         help='Host for action')
     parser.add_argument('-b', action='store', dest='boot', default="cloudimg",
@@ -222,12 +237,10 @@ def main():
                         help='Lab ID in configuration, default=lab1')
     parser.add_argument('-g', action='store', dest='cpus', type=int, default=1,
                         help='How many cpu for compute servers')
-    parser.add_argument('-d', action='store', dest='img_dir', default=None,
+    parser.add_argument('-d', action='store', dest='img_dir', default="/opt/imgs",
                         help='Where to store all images and disks, full abs path')
-    parser.add_argument('-z', action='store', dest='ubuntu_img_dir', default=None,
+    parser.add_argument('-z', action='store', dest='ubuntu_img_dir', default="/opt/iso",
                         help='Where to find downloaded cloud image, full abs path')
-
-
 
     opts = parser.parse_args()
     user = opts.user
@@ -236,15 +249,18 @@ def main():
     compute_servers_len = opts.computes
     lab_id = opts.labid
     compute_cpus = opts.cpus
-    IMG_PATH = opts.img_dir or IMG_PATH_LOCAL
-    ubuntu_img_path = opts.ubuntu_img_dir or ubuntu_img_path_local
+    img_path = opts.img_dir
+    ubuntu_img_path = opts.ubuntu_img_dir
     conn = libvirt.open('qemu+ssh://{user}@{host}/system'.format(user=user, host=host))
 
     with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "templates", "box.yaml")) as f:
         conf = yaml.load(f)
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "templates", "env.yaml")) as f:
+        env = yaml.load(f)
 
     ## TODO: move everything to configs
-    IP_START = 2
+    ip_start = env[lab_id]['ip_start']
+    net_start = env[lab_id]['net_start']
     lab_boxes = {
         "compute-servers": [],
         "build-server": [],
@@ -254,22 +270,26 @@ def main():
     ######### NETWORKS
 
     networks = conf['params']['networks']
+    for num, net in enumerate(networks):
+        net['net-ip'] = "192.168." + str(net_start + num)
+        net['name'] = lab_id + "-" + net['name']
 
     dhcp_hostline = '<host mac="{server_mac}" name="{server_name}.domain.name" ip="{server_ip}" />'
     mac = rand_mac()
-    ip = networks["boot"]["net-ip"] + "." + str(IP_START)
+    ip = networks[0]["net-ip"] + "." + str(ip_start)
     dhcp_records = dhcp_hostline.format(
         server_mac=mac,
         server_name="build-server",
         server_ip=ip)
     lab_boxes["build-server"].append({
-            "hostname": "build-server",
-            "name": lab_id + "-build-server",
-            "ip": ip,
-            "mac": mac})
+        "hostname": "build-server",
+        "name": lab_id + "-build-server",
+        "ip": ip,
+        "mac": mac
+        })
     for i in xrange(control_servers_len):
         mac = rand_mac()
-        ip = networks["boot"]["net-ip"] + "." + str(IP_START + i + 1)
+        ip = networks[0]["net-ip"] + "." + str(ip_start + i + 1)
         dhcp_records += "\n" + dhcp_hostline.format(
             server_mac=mac,
             server_name="control-server%.2d" % i,
@@ -279,10 +299,10 @@ def main():
             "name": lab_id + "-control-server%.2d" % i,
             "ip": ip,
             "mac": mac})
-    IP_START += control_servers_len
+    ip_start += control_servers_len
     for i in xrange(compute_servers_len):
         mac = rand_mac()
-        ip = networks["boot"]["net-ip"] + "." + str(IP_START + i + 1)
+        ip = networks[0]["net-ip"] + "." + str(ip_start + i + 1)
         dhcp_records += "\n" + dhcp_hostline.format(
             server_mac=mac,
             server_name="compute-server%.2d" % i,
@@ -292,66 +312,65 @@ def main():
             "name": lab_id + "-compute-server%.2d" % i,
             "ip": ip,
             "mac": mac})
-    IP_START += compute_servers_len
+    ip_start += compute_servers_len
 
-    for n in networks.itervalues():
+    for n in networks:
         try:
             net_undefine(conn, n['name'])
         except:
             print "not found net", n['name']
-    for net in networks.itervalues():
+    for net in networks:
         net_xml = net['xml'].format(name=net['name'], net_ip=net["net-ip"], dhcp_records=dhcp_records)
         net_create(conn, net_xml)
 
     ############ BOXES ############
-    remove_all_imgs()
+    remove_all_imgs(img_path, lab_id)
+    pool_name = "default-" + lab_id
     if opts.boot == "net":
-        if pool_found(conn, "default"):
-            pool_delete(conn, "default")
+        if pool_found(conn, pool_name):
+            pool_delete(conn, pool_name)
         pool_create(conn, conf['params']['pool']['xml'].format(
-            name="default", path=IMG_PATH))
+            name=pool_name, path=img_path))
 
-
-
-    new_img_path = os.path.join(IMG_PATH, "lab-backing.img")
+    new_img_path = os.path.join(img_path, lab_id + "-backing.img")
     convert_image(ubuntu_img_path, new_img_path)
 
     # create build server
     yaml_config = create_seed_yaml(conf, box=lab_boxes["build-server"][0]["hostname"])
-    disk=main_disk_create(lab_boxes["build-server"][0]["name"],
-                          10, conf, boot_type=opts.boot,
-                          user_seed_yaml=yaml_config,
-                          img_uncomp_path=new_img_path)
+    disk = main_disk_create(lab_boxes["build-server"][0]["name"],
+                            10, conf, boot_type=opts.boot, img_path=img_path,
+                            user_seed_yaml=yaml_config,
+                            img_uncomp_path=new_img_path)
     vm_xml = conf["params"]["build-server.xml"].format(
         name=lab_boxes["build-server"][0]["name"],
         ram=4*1024*1024,
         disk=disk,
-        net_boot_name=conf["params"]["networks"]["boot"]["name"],
+        net_boot_name=conf["params"]["networks"][0]["name"],
         build_server_mac=lab_boxes["build-server"][0]["mac"],
-        net_admin_name=conf["params"]["networks"]["admin"]["name"],
+        net_admin_name=conf["params"]["networks"][1]["name"],
     )
     delete_vm(conn, lab_boxes["build-server"][0]["name"])
     create_vm(conn, vm_xml)
-    print "Created build-server", lab_boxes["build-server"][0]["name"], "with ip",lab_boxes["build-server"][0]["ip"]
+    print "Created build-server", lab_boxes["build-server"][0]["name"], "with ip", lab_boxes["build-server"][0]["ip"]
 
     ### create other servers
     for compute in lab_boxes["compute-servers"]:
         yaml_config = create_host_seed_yaml(conf, box=compute, btype="compute-server")
         disk = main_disk_create(compute["name"],
-            10, conf, boot_type=opts.boot,
-            user_seed_yaml=yaml_config,
-            img_uncomp_path=new_img_path)
+                                10, conf, boot_type=opts.boot, img_path=img_path,
+                                user_seed_yaml=yaml_config,
+                                img_uncomp_path=new_img_path)
         vm_xml = conf["params"]["compute-server.xml"].format(
             name=compute["name"],
             ram=4*1024*1024,
             disk=disk,
             compute_server_cpu=compute_cpus,
-            net_boot_name=conf["params"]["networks"]["boot"]["name"],
+            net_boot_name=conf["params"]["networks"][0]["name"],
             mac=compute["mac"],
-            net_admin_name=conf["params"]["networks"]["admin"]["name"],
-            net_public_name=conf["params"]["networks"]["public"]["name"],
-            net_internal_name=conf["params"]["networks"]["internal"]["name"],
-            net_external_name=conf["params"]["networks"]["external"]["name"],
+            net_admin_name=conf["params"]["networks"][1]["name"],
+            net_public_name=conf["params"]["networks"][2]["name"],
+            net_internal_name=conf["params"]["networks"][3]["name"],
+            net_external_name=conf["params"]["networks"][4]["name"],
         )
         delete_vm(conn, compute["name"])
         create_vm(conn, vm_xml)
@@ -360,26 +379,24 @@ def main():
     for control in lab_boxes["control-servers"]:
         yaml_config = create_host_seed_yaml(conf, box=control, btype="control-server")
         disk = main_disk_create(control["name"],
-            10, conf, boot_type=opts.boot,
-            user_seed_yaml=yaml_config,
-            img_uncomp_path=new_img_path)
+                                10, conf, boot_type=opts.boot, img_path=img_path,
+                                user_seed_yaml=yaml_config,
+                                img_uncomp_path=new_img_path)
         vm_xml = conf["params"]["control-server.xml"].format(
             name=control["name"],
             ram=4*1024*1024,
             disk=disk,
             compute_server_cpu=compute_cpus,
-            net_boot_name=conf["params"]["networks"]["boot"]["name"],
+            net_boot_name=conf["params"]["networks"][0]["name"],
             mac=control["mac"],
-            net_admin_name=conf["params"]["networks"]["admin"]["name"],
-            net_public_name=conf["params"]["networks"]["public"]["name"],
-            net_internal_name=conf["params"]["networks"]["internal"]["name"],
-            net_external_name=conf["params"]["networks"]["external"]["name"],
+            net_admin_name=conf["params"]["networks"][1]["name"],
+            net_public_name=conf["params"]["networks"][2]["name"],
+            net_internal_name=conf["params"]["networks"][3]["name"],
+            net_external_name=conf["params"]["networks"][4]["name"],
         )
         delete_vm(conn, control["name"])
         create_vm(conn, vm_xml)
         print "Created control-server", control["name"], "with ip", control["ip"]
-
-
     #boxes = conn.listAllDomains()
     #for b in boxes:
     #    print b.name()
