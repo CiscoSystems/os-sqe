64c082b3e5f9093ba24d255c963791f74b45ced3
Sergey Sh <einarum@gmail.com>
2014-08-02 13:31:38 +0400

    Fix retesting failed tests

--- tools/tempest-scripts/run_tempest_tests.sh
+++ tools/tempest-scripts/run_tempest_tests.sh
@@ -3,14 +3,17 @@ source $WORKSPACE/tempest/.venv/bin/activate
 source $WORKSPACE/openstack-sqe/openrc
 tests="$WORKSPACE/openstack-sqe/tools/tempest-scripts/tests_set"
 cd $WORKSPACE/tempest/
-rm -rf .testrepository || :
-testr init || :
-if [ -s "$tests" ]; then
-    testr run --load-list "$tests" --subunit  | subunit-2to1 | tools/colorizer.py || :
+#rm -rf .testrepository || :
+if [ ! -d .testrepository ]; then
+    testr init || :
+    if [ -s "$tests" ]; then
+        testr run --load-list "$tests" --subunit  | subunit-2to1 | tools/colorizer.py || :
+    else
+        testr run "$REG" --subunit | subunit-2to1 | tools/colorizer.py || :
+    fi
 else
-    testr run "$REG" --subunit | subunit-2to1 | tools/colorizer.py || :
+    testr run --failing --subunit | subunit-2to1 | tools/colorizer.py || :
 fi
-
 results=${WORKSPACE}/openstack-sqe/nosetests_$(date  +%s).xml
 testr last --subunit | subunit-1to2 | subunit2junitxml --output-to="$results" || :
 testr last --subunit > "${WORKSPACE}/openstack-sqe/testr_results.subunit"
