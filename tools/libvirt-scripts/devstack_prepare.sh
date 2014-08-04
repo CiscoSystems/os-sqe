2295a8266ffcd3f67201ed7613176b4984c7c1e5
Sergey Shnaidman <sshnaidm@cisco.com>
2014-08-04 17:18:43 +0400

    Devstack tempest prepare and snapshots

---
+++ tools/libvirt-scripts/devstack_prepare.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ ! "$1" ]; then
+    labid=$1
+else if [ ! "$LAB" ]; then
+    labid=$LAB
+else
+    echo "No \$LAB variable id defined"
+    exit 1
+fi
+fi
+
+if [ ! "$WORKSPACE" ]; then
+    this_=$(readlink -f $0)
+    this_dir=$(dirname $this_)
+    WORKSPACE="${this_dir}/../../.."
+fi
+
+export DEV_IP=$(sed -n "/${labid}:/{n;p;n;p;}" tools/cloud/cloud-templates/lab.yaml | sed 'N;s/\n/ /' | sed "s/    ip_start: /./g" | sed "s/   net_start: //g")
+
+scp localadmin@$DEV_IP:/opt/stack/tempest/etc/tempest.conf $WORKSPACE/openstack-sqe/tempest.conf
+if [ -d "$WORKSPACE/tempest/etc" ]; then
+cp $WORKSPACE/openstack-sqe/tempest.conf $WORKSPACE/tempest/etc/
+fi
+scp localadmin@$DEV_IP:~/devstack/openrc $WORKSPACE/openrc && cp $WORKSPACE/openrc $WORKSPACE/openstack-sqe/openrc
