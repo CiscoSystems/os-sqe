14e822b995e416eed983c539ddb73a635ab36987
Nikolay Fedotov <nfedotov@cisco.com>
2014-09-09 15:35:08 +0400

    Script for virtual host image

--- neutron_ci/nodepool-scripts/prepare_node.sh
+++ neutron_ci/nodepool-scripts/prepare_node.sh
@@ -2,22 +2,13 @@
 
 WORK_DIR=`pwd`
 
-cat >>/home/ubuntu/.ssh/authorized_keys <<EOF
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCj9HsmH5E2hlwOXpBd8FFQY0fxMBJBiFeAc/I9ds9WQMjeuLnYWICE2DCX7AoMSwPLl/IwKQZGV/Vt3nB12/EKvTMx6yCgfPOGrejzYYUhDSxJwuFg5KHDuLHXcUjDz/uAn2mzEDwPqtsxRSeKB/IYa7cn2VdgmrIi7PPc2/Gzk7CnYpqwMjMr1A9BxJz41yCN4gkNxk8LFxVtiAFQRuPVAU06yzEbQHPzPCZbVOpBo+TZcEOIepCy0DLrjcwEa26uBEeN13aGWaRuhtztYngtrpK1d4ivVGprYjvrpRGinYP2ETaX13UtseSz4pKnip/JbL+kCkuAxD3/UloK3v5p nfedotov@cisco.com
-EOF
+bash ./prepare_jenkins_node.sh
 
 ssh-keygen -P '' -f /home/ubuntu/.ssh/id_rsa
 cat /home/ubuntu/.ssh/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
 
-echo "apt_preserve_sources_list: true" | sudo tee /etc/cloud/cloud.cfg.d/99-local-mirror-only.cfg
-sudo cp sources.list /etc/apt/
-sudo apt-get update
-
-# Install java
-sudo apt-get install -y openjdk-6-jre
-
 # Install packages
-sudo apt-get install -y python-pip libxml2-dev libxslt1-dev python-dev zlib1g-dev sshpass git mysql-client libmysqlclient-dev
+sudo apt-get install -y python-pip libxml2-dev libxslt1-dev python-dev zlib1g-dev sshpass mysql-client libmysqlclient-dev
 sudo pip install ecdsa junitxml
 
 # Install Cisco ncclient
