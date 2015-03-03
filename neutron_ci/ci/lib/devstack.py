db4084f31ef8c8e06183a971734f74ca731cf29c
nfedotov <nfedotov@cisco.com>
2015-03-03 11:44:14 +0300

    Typo

--- neutron_ci/ci/lib/devstack.py
+++ neutron_ci/ci/lib/devstack.py
@@ -195,7 +195,7 @@ class DevStack(object):
             logger.info('Remove /opt/stack folder')
             run('sudo rm -rf /opt/stack')
             logger.info('Call "sudo apt-get autoremove"')
-            run('sudo apt-get autoremove')
+            run('sudo apt-get autoremove -y')
 
     def restart_ovs(self):
         with settings(host_string=self.host_string, warn_only=True):
