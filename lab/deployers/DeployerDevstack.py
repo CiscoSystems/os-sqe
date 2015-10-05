de9aff761834c8287db069ba9fde7494cef47069
Kirill Shileev <kshileev@gmail.com>
2015-10-05 21:39:57 +0300

    fixes: 1. problem with verify cloud when deployed by libvirt 2. the range of ipmi ips for G10 lab
    Change-Id: I1a377c9ea949428ffe1e9a005654fd45d952b75c

--- lab/deployers/DeployerDevstack.py
+++ lab/deployers/DeployerDevstack.py
@@ -47,6 +47,7 @@ class DeployerDevstack(Deployer):
         if not controllers:
             raise ErrorDeployerDevstack('No aio or controller provided!')
         self.roles = controllers + others  # this is to ensure that controllers are deployed first
+        self.actual_servers = []  # deployment will run on this list
 
     @staticmethod
     def fill_devstack_config(config_as_string, cloud_status):
@@ -74,6 +75,7 @@ class DeployerDevstack(Deployer):
         repo = self.clone_repo(repo_url='https://git.openstack.org/openstack-dev/devstack.git', server=server)
         self.put(what=config_as_string, name='local.conf', in_directory=repo, server=server)
         self.run(command='./unstack.sh', in_directory=repo, warn_only=True, server=server)
+        self.run(command='./clean.sh', in_directory=repo, warn_only=True, server=server)
         self.run(command='sudo pkill --signal 9 -f python', warn_only=True, server=server)  # Kill all python processes to avoid errors: Address already in use
         self.run(command='./stack.sh', in_directory=repo, server=server)
 
@@ -81,20 +83,20 @@ class DeployerDevstack(Deployer):
         from lab.Cloud import Cloud
 
         cloud = Cloud(cloud=self.cloud, user=self.user, tenant=self.tenant, admin='admin', password=self.password)
-        actual_servers = []  # deployment will run on this list
         hostname_vs_server = {server.hostname: server for server in servers}  # {aio: server_obj, ...}
         for role in self.roles:  # [{'hostname': 'aio', 'config_name': 'aio', 'config_as_string': 'blalbl'}, ...]
             if role['hostname'] not in hostname_vs_server.keys():
                 raise ErrorDeployerDevstack('Expected server "{0}" which is not provided'.format(role['hostname']))
             server = hostname_vs_server[role['hostname']]
             cloud.add_server(config_name=role['config_name'], server=server)
-            actual_servers.append((role['config_as_string'], server))
+            server.config_as_string = role['config_as_string']
+            self.actual_servers.append(server)
 
-        for config_before_fill, server in actual_servers:
-            config_after_fill = self.fill_devstack_config(config_as_string=config_before_fill, cloud_status=cloud)
+        for server in self.actual_servers:
+            config_after_fill = self.fill_devstack_config(config_as_string=server.config_as_string, cloud_status=cloud)
             self.run_devstack_on_server(server=server, config_as_string=config_after_fill)
         return cloud
 
     def wait_for_cloud(self, list_of_servers):
         cloud = self.deploy_cloud(servers=list_of_servers)
-        return self.verify_cloud(cloud=cloud, from_server=self.roles[0])
+        return self.verify_cloud(cloud=cloud, from_server=self.actual_servers[0])
