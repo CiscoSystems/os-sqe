5eb1d3edac62accd1de4a6feec42e0074aaaab3c
Kirill Shileev <kshileev@gmail.com>
2015-10-22 19:49:26 +0300

    fix errors with generatin report
    Change-Id: I1e5a894dc1c062b7672677cdaff953599e264c0c

--- lab/runners/__init__.py
+++ lab/runners/__init__.py
@@ -10,13 +10,13 @@ class Runner(WithConfig, WithRunMixin):
 
     @staticmethod
     def get_artefacts(server):
-        configs = ['version:\n{0}'.format(server.run('rpm -qi rpm -qi python-networking-cisco'))]
+        configs = ['version:\n{0}'.format(server.run('rpm -qi python-networking-cisco'))]
         for x in ['vlan_ranges', 'ucsm', 'api_workers']:
             cmd = 'sudo grep -r {0} /etc/neutron/* | grep -v \#'.format(x)
             configs.append('\n{0} gives:\n {1}'.format(cmd, server.run(cmd)))
 
         cmd = 'sudo grep -i ERROR /var/log/neutron/* | grep -i ucsm'
-        configs.append('\n{0} gives:\n {1}'.format(cmd, server.run(cmd)))
+        configs.append('\n{0} gives:\n {1}'.format(cmd, server.run(cmd, warn_only=True)))
 
         with open('configs-logs.txt', 'w') as f:
             f.write('\n\n'.join(configs))
