4e76890e9fdb9633f370e87d261d8dcc94f9038c
Nikolay Fedotov <nfedotov@cisco.com>
2015-12-10 09:11:10 +0000

    HA: scenario for running tempest test
    Change-Id: I67c290fcec7e4fd5186ba221c2eaff11e98e0d22

---
+++ lab/scenarios/tempest_test.py
@@ -0,0 +1,13 @@
+import datetime
+from fabric.api import local, cd
+
+
+def main(context, log, args):
+    test = args['test']
+    etime = args['etime']
+    tempest_path = args['tempest_path']
+
+    start_time = datetime.datetime.now()
+    with cd(tempest_path):
+        while (datetime.datetime.now() - start_time).seconds < etime:
+            local('source ~/VE/tempest/bin/activate && testr run {0}'.format(test))
