f44b8ed63ad7927d833a28dbd2a969c12544805f
Abishek Subramanian <absubram@cisco.com>
2015-12-15 15:16:56 -0500

    Fixes for BXB testbed
    Fix end_point typo and also fixes the end_point IP address.
    
    Change-Id: If615669ba4db6fa6da52e024f559eeadae3058aa

--- lab/deployers/DeployerExisting.py
+++ lab/deployers/DeployerExisting.py
@@ -8,7 +8,7 @@ class ErrorDeployerExisting(Exception):
 class DeployerExisting(Deployer):
 
     def sample_config(self):
-        return {'cloud': 'arbitrary name', 'end-point': 'http of cloud end_point',
+        return {'cloud': 'arbitrary name', 'end_point': 'http of cloud end_point',
                 'user': 'default user', 'tenant': 'tenant name', 'admin': 'admin username', 'password': 'password for both'}
 
     def __init__(self, config):
@@ -16,7 +16,7 @@ class DeployerExisting(Deployer):
 
         super(DeployerExisting, self).__init__(config=config)
         self.cloud_end_point_ip = config['end_point']
-        self.cloud = Cloud(cloud=config['cloud'], user=config['user'], tenant=config['tenant'], admin=config['admin'], password=config['password'], end_point=config['end-point'])
+        self.cloud = Cloud(cloud=config['cloud'], user=config['user'], tenant=config['tenant'], admin=config['admin'], password=config['password'], end_point=config['end_point'])
 
     def wait_for_cloud(self, list_of_servers):
         return self.verify_cloud(cloud=self.cloud, from_server=list_of_servers[0])
