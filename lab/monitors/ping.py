d0c587a56b53df879c1e8fbe1eeb21e465ce2fe4
Kirill Shileev <kshileev@gmail.com>
2015-12-15 20:01:09 +0300

    fix bug with wrong down time calculation
    Change-Id: I8f9d7a0ab14b87095e5b140327906935ffb16e42

--- lab/monitors/ping.py
+++ lab/monitors/ping.py
@@ -1,5 +1,5 @@
 def start(context, log, args):
-    from fabric.api import local, settings
+    from fabric.api import local, settings, hide
     import time
 
     down_start = down_end = None
@@ -8,13 +8,18 @@ def start(context, log, args):
         ucsm_ip = context.ucsm_ip()
         try:
             with settings(warn_only=False):
-                local('ping -c 1 -W 1 {0}'.format(ucsm_ip))
-            if down_start:
+                with hide('running', 'stdout', 'stderr'):
+                    local('ping -c 1 -W 1 {0}'.format(ucsm_ip))
+            if down_start and not down_end:
                 down_end = time.time()
-            log.info('{0} is alive'.format(ucsm_ip))
+            log.info('{0} is 1'.format(ucsm_ip))
         except:
-            down_start = time.time()
-            log.info('{0} is down'.format(ucsm_ip))
+            if not down_start:
+                down_start = time.time()
+            log.info('{0} is 0'.format(ucsm_ip))
         time.sleep(args['period'])
     if down_start:
-        log.info('Down time is {0} secs from {1} to {2}'.format(down_end - down_start, down_start, down_end))
+        if down_end:
+            log.info('Down time is {0} secs from {1} to {2}'.format(down_end - down_start, down_start, down_end))
+        else:
+            log.info('Down time from {0} and still down when monitor is finished'.format(down_start))
