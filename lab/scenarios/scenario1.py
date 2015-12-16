f36aa87c504bab2a04c8487b9724f211b7ed7aca
Kirill Shileev <kshileev@gmail.com>
2015-12-16 15:50:03 +0300

    change logging
    Change-Id: I2310b1c822ce4f74f1932d7bfe3e4c704d1dbddb

--- lab/scenarios/scenario1.py
+++ lab/scenarios/scenario1.py
@@ -1,6 +1,5 @@
 def test(iteration, log, is_cleanup):
     from fabric.api import local, settings
-    import datetime
     import re
 
     id = lambda result: re.search('id\s+\|\s+([-\w]+)', ''.join(res.stdout)).group(1)
@@ -10,7 +9,6 @@ def test(iteration, log, is_cleanup):
     subnet_id = None
     port_id = None
     try:
-        log.info("######### Start time: " + str(datetime.datetime.now()))
         res = local('neutron net-create {0}'.format(name), capture=True)
         net_id = id(res)
         res = local('neutron subnet-create {0} 10.0.100.0/24'.format(net_id), capture=True)
@@ -21,7 +19,7 @@ def test(iteration, log, is_cleanup):
             local('neutron port-delete {0}'.format(port_id))
             local('neutron subnet-delete {0}'.format(subnet_id))
             local('neutron net-delete {0}'.format(net_id))
-        log.info("######### End time: " + str(datetime.datetime.now()))
+        log.info('{0} net-subnet-port created'.format(iteration))
     except:
         with settings(warn_only=False):
             if port_id:
@@ -31,6 +29,9 @@ def test(iteration, log, is_cleanup):
 
 
 def start(context, log, args):
-    for id in xrange(0, args['times']):
-        test(id, log, is_cleanup=args.get('cleanup', True))
+    import time
 
+    start_time = time.time()
+    for i in xrange(0, args['times']):
+        test(iteration=i, log=log, is_cleanup=args.get('cleanup', True))
+    log.info('{0} net-subnet-ports created in {1} secs'.format(args['times'], time.time()-start_time))
