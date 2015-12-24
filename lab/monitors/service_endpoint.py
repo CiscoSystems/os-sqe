98f74ed71353dcfb7854d64646571e9e2a4612e0
Kirill Shileev <kshileev@gmail.com>
2015-12-24 17:18:01 +0300

    use services from cloud
    Change-Id: Ibe40d1416760d9f9eebf158f1b62f5c1d04ba552

--- lab/monitors/service_endpoint.py
+++ lab/monitors/service_endpoint.py
@@ -1,24 +1,17 @@
-def start(context, log, args):
+def start(lab, log, args):
     import time
     import requests
 
-    from fabric.api import local
-
-    # service type: compute, network, image etc.
-    type = args['type']
-    # adminURL / publicURL / internalURL
-    url_type = args.get('url_type', 'publicURL')
+    duration = args['duration']
+    period = args['period']
+    service = args['service']      # service type: compute, network, image etc.
+    url = args.get('url_type', 'publicURL')     # adminURL / publicURL / internalURL
 
     # Finding out the service endpoint
-    endpoint = local("keystone endpoint-get --service {0} | grep {1} | awk '{print $4}'".format(type, url_type), capture=True)
-    if not endpoint:
-        raise Exception('Could not start monitoring. '
-                        'There is not such service type "{0}"'.format(type))
-
-    log.info('Starting monitoring response from {0}'.format(endpoint))
+    endpoint = lab.cloud.get_service_end_point(service, url)
 
     start_time = time.time()
-    while start_time + args['duration'] > time.time():
+    while start_time + duration > time.time():
         try:
             # Trying to get response from service
             requests.get(endpoint)
@@ -26,6 +19,16 @@ def start(context, log, args):
         except requests.exceptions.ConnectionError:
             res = 0
 
-        log.info('Service {0}, endpoint {1}. Status {2}'.format(type, endpoint, res))
+        log.info('Service {0}, endpoint {1}. Status {2}'.format(service, endpoint, res))
 
-        time.sleep(args['period'])
+        time.sleep(period)
+
+if __name__ == '__main__':
+    from lab.laboratory import Laboratory
+    from lab.logger import create_logger
+    from lab.cloud import Cloud
+
+    cloud = Cloud(cloud='g10', user='admin', admin='admin', tenant='admin', password='w32utrzAEaJJHZZqpr6VRPKaZ', end_point='http://10.23.230.132:5000/v2.0/')
+    lab = Laboratory('g10.yaml')
+    lab.cloud = cloud
+    start(lab=lab, log=create_logger('name'), args={'service': 'network', 'duration': 20, 'period': 2})
