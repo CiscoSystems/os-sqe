e95dcb158691211f0b3db336ffc7288c0f28620c
Eugene Bagdasaryan <ebagdasa@cisco.com>
2015-12-31 09:38:15 +0300

    Fixes according to the actual timing
    Change-Id: I63f29c8ab8c6a0b697e304322b4193df9caef86a

--- lab/scenarios/delete.py
+++ lab/scenarios/delete.py
@@ -6,7 +6,19 @@ def start(lab, log, args):
 
     start_time = time.time()
     for ob_type in ['port', 'net']:
-        for oid in server.run(command='neutron {o}-list {l} | grep {u} | awk \'{{print $2}}\''.format(o=ob_type, l=lab.cloud, u=unique_pattern_in_name)).split('\n'):
+        res = server.run(command='neutron {o}-list {l} | grep {u} | awk \'{{print $2}}\''.format(o=ob_type, l=lab.cloud, u=unique_pattern_in_name)).split('\n')
+        if ob_type == "net":
+            i = len(res)
+        for oid in res:
             if oid:
-                server.run(command='neutron {ob}-delete {id} {lab_creds}'.format(ob=ob_type, id=oid.strip(), lab_creds=lab.cloud))
+                res = server.run(command='neutron {ob}-delete {id} {lab_creds}'.format(ob=ob_type, id=oid.strip(),
+                                                                                       lab_creds=lab.cloud), warn_only=True)
+                # Sometimes neutron timeouts, rerun the command if so
+                if not res:
+                    time.sleep(60)
+                    res = server.run(command='neutron {ob}-delete {id} {lab_creds}'.format(ob=ob_type, id=oid.strip(),
+                                                                                       lab_creds=lab.cloud), warn_only=True)
+                if ob_type == "net":
+                    log.info("{0} network-deleted".format(i))
+                    i -= 1
     log.info('All net-subnet-ports deleted in {0} secs'.format(time.time()-start_time))
