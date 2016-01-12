717b7b296d5e48ca07dfb31ce00aed967debb2bc
Abishek Subramanian <absubram@cisco.com>
2016-01-12 15:30:27 -0500

    Updates for BXB rally
    Change-Id: Ifde9696888792a113cfce7792306b601b2e26b1a

--- lab/runners/rally.py
+++ lab/runners/rally.py
@@ -9,7 +9,7 @@ def create_task(concurrency, max_vlans, is_single_point):
     start = n_tenants if is_single_point else 1
     end = n_tenants + 1
     step = 10000 if is_single_point else n_tenants / 10
-    with open('lab/configs/rally/scaling.yaml') as f:
+    with open('configs/rally/scaling.yaml') as f:
         task_body = f.read()
         task_body = task_body.replace('{concurrency}', concurrency)
         task_body = task_body.replace('{n_tenants_start_end_step}', '{0}, {1}, {2}'.format(start, end, step))
