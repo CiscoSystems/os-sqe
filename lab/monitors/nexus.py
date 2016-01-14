2b97f8c7ed5da05b3967fdaef1dd0eee0e8848c2
Kirill Shileev <kshileev@gmail.com>
2016-01-14 20:31:27 +0300

    refactor nexus monitor
    Change-Id: I13d96e6030504ebfcf67cdaa5900a6c7b50454f9

--- lab/monitors/nexus.py
+++ lab/monitors/nexus.py
@@ -1,73 +1,38 @@
-def start(context, log, args):
+def start(lab, log, args):
     import time
-    import logging
-    import json
-    import requests
+    import validators
+    from lab.providers import n9k
 
-    nexus_name = args.get('name', 'Nexus_'.format(args['ip']))
+    duration = args['duration']
+    period = args['period']
+    name_or_ip = args['name_or_ip']
+    is_show_details = args.get('is_show_details', False)
 
-    def _nxapi(commands):
-        request = [{"jsonrpc": "2.0", "method": "cli", "params": {"cmd": command, "version": 1}, "id": 1} for command in commands]
-        try:
-            results = requests.post('http://{0}/ins'.format(args['ip']), auth=(args['username'], args['password']),
-                                    headers={'content-type': 'application/json-rpc'}, data=json.dumps(request)).json()
-            for i, x in enumerate(results, start=0):
-                if 'error' in x:
-                    raise Exception('Error: {0} in command: {1}'.format(x['error']['data']['msg'].strip('\n'), commands[i]))
-            return results
-        except:
-            logging.exception("Exception while executing nexus command")
-            return {}
+    if validators.ipv4(name_or_ip):
+        n9k_ip = name_or_ip
+        n9k_username = args['username']
+        n9k_password = args['password']
+    else:
+        n9k_ip, _, n9k_username, n9k_password = lab.n9k_creds()
 
-    def _make_vlans_set(vlans_str):
-        """
-        Converts alllowed vlans string to a set object
-        :param self:
-        :param vlans_str: Ex: 1,177,2006,3000-3004
-        :return: Set object {1, 177, 2006, 3000, 3001, 3002, 3003, 3004}
-        """
-        vlans = set()
-        for vlan_range in vlans_str.split(','):
-            se = vlan_range.split('-')
-            if len(se) == 2:
-                vlans = vlans | set(range(int(se[0]), int(se[1]) + 1))
-            elif len(se) == 1:
-                vlans.add(int(se[0]))
-        return vlans
+    nx = n9k.Nexus(n9k_ip, n9k_username, n9k_password)
 
-    def _get_item(dictionary, path, default=None):
-        """
-        Looks for value in dictionary.
-        :param dictionary: Source
-        :param path: Path to value. List object
-        :param default: Default value if there is no such path
-        :return: Found value
-        """
-        d = dictionary
-        for i in range(0, len(path) - 1):
-            d = d.get(path[i], {})
-        return d.get(path[-1], default)
+    port_channels = nx.show_port_channel_summary()
 
-    log.info('Starting NXOS monitoring {0}'.format(nexus_name))
     start_time = time.time()
-
-    port_channels = _get_item(_nxapi(['show port-channel summary']), ['result', 'body', 'TABLE_channel', 'ROW_channel'], [])
-
-    while start_time + args['duration'] > time.time():
+    while start_time + duration > time.time():
         # Allowed vlans
         for port_channel in port_channels:
-            pc_name = port_channel['port-channel']
-            cmd = 'show interface {0} switchport'.format(pc_name)
-            allowed_vlans = _nxapi([cmd])['result']['body']['TABLE_interface']['ROW_interface']['trunk_vlans']
-            log.info('{0} {1} Allowed vlans: {2}'.format(nexus_name, pc_name, _make_vlans_set(allowed_vlans)))
+            allowed_vlans = nx.show_interface_switchport(name=port_channel)
+            log.info('ip={ip} service={srv} n_vlans={n} {det}'.format(ip=n9k_ip, srv=port_channel, n=len(allowed_vlans),
+                                                                      det='list={0}'.format(allowed_vlans) if is_show_details else ''))
 
         # Vlans
-        vlans = _get_item(_nxapi(['show vlan']), ['result', 'body', 'TABLE_vlanbrief', 'ROW_vlanbrief'], [])
-        log.info('nexus={0} vlans:={1}'.format(nexus_name, vlans))
+        vlans = nx.show_vlan()
+        log.info('ip={ip} n_vlans={n} {det}'.format(ip=n9k_ip, n=len(vlans),
+                                                    det='list={0}'.format(vlans) if is_show_details else ''))
 
         # User sessions
-        users = _get_item(_nxapi(['show users']), ['result', 'body', 'TABLE_sessions', 'ROW_sessions'], [])
-        users = [users] if isinstance(users, dict) else users
-        log.info('{0} User sessions: {1}'.format(nexus_name, map(lambda u: u['p_pid'], users)))
-
-        time.sleep(args['period'])
+        users = nx.show_users()
+        log.info('ip={ip} sessions={s}'.format(ip=n9k_ip, s=users))
+        time.sleep(period)
--- lab/test.py
+++ lab/test.py
@@ -2,9 +2,9 @@ if __name__ == '__main__':
     from lab.laboratory import Laboratory
     from lab.logger import create_logger
     from lab.cloud import Cloud
-    from lab.monitors import service_endpoint as tst
+    from lab.monitors import nexus as tst
 
     cloud = Cloud(cloud='g10', user='admin', admin='admin', tenant='admin', password='w32utrzAEaJJHZZqpr6VRPKaZ', end_point='http://10.23.230.132:5000/v2.0/')
     lab = Laboratory('g10.yaml')
     lab.cloud = cloud
-    tst.start(lab=lab, log=create_logger('name'), args={'service': 'network', 'duration': 20, 'period': 2})
+    tst.start(lab=lab, log=create_logger('test'), args={'name_or_ip': 'heh', 'duration': 20, 'period': 2})
