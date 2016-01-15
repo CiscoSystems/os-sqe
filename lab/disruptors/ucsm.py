ce95294f06e4f0411626b897a10b5e2749e0738e
Nikolay Fedotov <nfedotov@cisco.com>
2016-01-15 11:53:56 +0000

    UCSM Reboot Disruptor
    Change-Id: I21cdb80c2387fd0a408afaa827693c2af2e91ef7

--- lab/disruptors/ucsm.py
+++ lab/disruptors/ucsm.py
@@ -1,9 +1,43 @@
 def start(context, log, args):
-    from fabric.api import settings, run
+    import os
+    import yaml
+    from fabric.api import run, settings
 
-    log.info('Rebooting...')
-    prompt = {'Before rebooting, please take a configuration backup.\nDo you still want to reboot? (yes/no):': 'yes'}
-    with settings(host_string='{user}@{ip}'.format(user=context.ucsm_username(), ip=context.ucsm_ip()), password=context.ucsm_password(), connection_attempts=50, warn_only=False):
-        run('connect local-mgmt', shell=False)
-        with settings(prompts=prompt):
-            run('reboot', shell=False)
+    # PRIMARY or SUBORDINATE
+    role = args.get('role', 'PRIMARY')
+
+    ip, user, password = context.ucsm_creds()
+
+    def call_ucsm(command):
+        with settings(host_string='{user}@{ip}'.format(user=user, ip=ip), password=password, connection_attempts=1, warn_only=False, timeout=1):
+            try:
+                return run(command, shell=False, quiet=True).stdout
+            except:
+                return ''
+
+    fabric_id = call_ucsm('show cluster state | include {0}'.format(role))[0].strip(':').lower()
+    fabric_ip = yaml.load(call_ucsm("show fabric-interconnect {0} detail | egrep 'OOB IP Addr:'".format(fabric_id)))['OOB IP Addr']
+
+    script = """#!/usr/bin/expect
+set user {user}
+set pass {password}
+set switch_ip {ip}
+
+spawn ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${{user}}@${{switch_ip}}
+expect "Password:" {{send "$pass\r"}}
+
+expect "#" {{send "connect local-mgmt\r"}}
+expect "(local-mgmt)#" {{send "reboot\r"}}
+expect "(yes/no):" {{send "yes\r"}}
+sleep 5
+exit
+""".format(ip=fabric_ip, user=user, password=password)
+    script_name = 'ucsm_reload.exp'
+
+    director = context.director()
+    script_path = os.path.join(director.temp_dir, script_name)
+
+    director.put(script, script_path, '/')
+    director.run('yum install -y expect')
+    res = director.run('expect {0}'.format(script_path), warn_only=True)
+    log.info('result={0}'.format(res.return_code))
