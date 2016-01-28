b8fddbe530ed5629b9694062422a886f50cba653
Nikolay Fedotov <nfedotov@cisco.com>
2016-01-28 12:29:56 +0000

    Configs for logstash and filebeat
    Change-Id: Ia24962a97d5e13847989e4cd579fcf4c2ebd218d

--- fabs/elk.py
+++ fabs/elk.py
@@ -5,10 +5,25 @@ from fabric.api import task
 def run_elk():
     """fab elk.run_elk \t\t\t\t Deploy elk stack as docker container."""
     from fabric.api import sudo
+    from fabric.operations import put
 
-    sudo('docker pull sebp/elk')
-    sudo('docker run -p 7777:5601 -p 9999:9200 -p 7744:5044 -p 7700:5000 -it --name elk sebp/elk')
+    image_name = 'sebp/elk'
+    container_name = 'elk'
+    remote_beats_input_conf = '/tmp/02-beats-input.conf'
 
+    get_container_id = lambda: sudo("docker ps | grep {0} | awk '{{print $1}}'".format(container_name)).stdout.strip()
+
+    sudo('docker pull {0}'.format(image_name))
+    container_id = get_container_id()
+    if not container_id:
+        sudo('docker run -p 7777:5601 -p 9999:9200 -p 7744:5044 -p 7700:5000 -it --name {0} {1}'.format(container_name, image_name), warn_only=True)
+        container_id = get_container_id()
+        container_id = get_container_id()
+
+    put('configs/elk/02-beats-input.conf', remote_beats_input_conf)
+    sudo('docker cp {0} {1}:/etc/logstash/conf.d/02-beats-input.conf'.format(remote_beats_input_conf, container_id))
+    sudo('docker exec {0} /opt/logstash/bin/plugin install logstash-filter-elapsed'.format(container_id))
+    sudo('docker exec {0} service logstash restart'.format(container_id))
 
 @task
 def json_to_es():
