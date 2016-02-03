2bbc54fac3031edfadb01b6280eb5746c27483d4
Kirill Shileev <kshileev@gmail.com>
2016-02-03 16:28:58 +0300

    add osp7 and jenkins info to logger
    Change-Id: I2a084634c963c6a78fbb8cd4afabbf082dfe6801

--- lab/logger.py
+++ lab/logger.py
@@ -18,7 +18,7 @@ class JsonFormatter(logging.Formatter):
     def format(self, record):
         import re
         import json
-        from lab import REPO_TAG
+        import lab
 
         d = {}
         for key_value in re.split(pattern='[ ,]', string=record.message):
@@ -38,7 +38,9 @@ class JsonFormatter(logging.Formatter):
         if '@timestamp' not in d:
             d['@timestamp'] = self.formatTime(record=record, datefmt="%Y-%m-%dT%H:%M:%S.000Z")
         d['name'] = record.name
-        d['repo_tag'] = REPO_TAG
+        d['repo_tag'] = lab.REPO_TAG
+        d['osp7_info'] = lab.OSP7_INFO
+        d['jenkins'] = lab.JENKINS_TAG
         if record.exc_text:
             d['EXCEPTION'] = record.exc_text.replace('\n', '')
         return json.dumps(d)
