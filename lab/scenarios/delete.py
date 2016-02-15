30b6ffc3c610b11f7d61ea41d1ad6acd6366fa20
Kirill Shileev <kshileev@gmail.com>
2016-02-15 17:23:49 +0300

    move n9k.py to package lab
    Change-Id: I3f31f6a2910f2ec74ccd9b4fd4223fa8fe9a4c34

--- lab/scenarios/delete.py
+++ lab/scenarios/delete.py
@@ -1,6 +1,6 @@
 def start(lab, log, args):
     import time
-    from lab.providers import n9k
+    from lab import n9k
     from lab import fi
 
     unique_pattern_in_name = args.get('unique_pattern_in_name', 'sqe-test')
