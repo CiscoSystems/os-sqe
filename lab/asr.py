10e32ee5d12f921d84c0729751342d91dcfaa5db
Kirill Shileev <kshileev@gmail.com>
2016-03-01 17:35:25 +0300

    implement ASr.cmd
    Change-Id: I58bffde68c9b024d9ec01f90592562e46b5f2cd6

--- lab/asr.py
+++ lab/asr.py
@@ -2,6 +2,8 @@ from lab_node import LabNode
 
 
 class Asr(LabNode):
+    def cmd(self, command):
+        from fabric.api import settings, run
 
-    def cmd(self, cmd):
-        return NotImplementedError
+        with settings(host_string='{user}@{ip}'.format(user=self._username, ip=self._ip), password=self._password, connection_attempts=50, warn_only=False):
+            return run(command, shell=False)
