8199525f2d2a43ab8ac12604766516456406a3c7
Evgeny Bagdasaryan <ebagdasa@cisco.com>
2016-03-03 17:14:49 +0300

    Parametrize VXLAN topology
    Change-Id: I5e5589a1b412c363d364e76a323689392838cf05

--- lab/asr.py
+++ lab/asr.py
@@ -8,5 +8,5 @@ class Asr(LabNode):
         with settings(host_string='{user}@{ip}'.format(user=self._username, ip=self._ip), password=self._password, connection_attempts=50, warn_only=False):
             return run(command, shell=False)
 
-    def configure_for_osp7(self):
+    def configure_for_osp7(self, topology):
         return None
