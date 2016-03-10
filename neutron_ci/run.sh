8d6fef0e0e583c2181c5ccb6a7a7c9ab0d95758a
Yaroslav Morkovnikov <ymorkovn@cisco.com>
2016-03-10 05:50:15 -0800

    Remove nginx

--- neutron_ci/run.sh
+++ neutron_ci/run.sh
@@ -7,11 +7,6 @@ publish_path=$3
 publish_login=$4
 publish_pass=$5
 
-sudo pip install --upgrade pip
-#Removing cache on the node
-echo "runnin sudo rm -f /home/ubuntu/.cache/pip/http"
-sudo rm -f /home/ubuntu/.cache/pip/http
-sudo pip install --upgrade ndg-httpsclient
 sudo pip install -r requirements.txt
 
 if [[ -n "${PARAMS_BASE}" ]]; then
