ba2427dc6a66e297a8155c020485fe89f5a8315d
Kirill Shileev <kshileev@gmail.com>
2016-03-24 15:47:10 +0300

    Allow rally runner to get rally from specified repo and apply specified patch
    Change-Id: Ic3b7e453e59e44874340aa7b679ee35adae0f3bf

--- lab/runners/runner_rally.py
+++ lab/runners/runner_rally.py
@@ -4,25 +4,26 @@ from lab.runners import Runner
 class RunnerRally(Runner):
 
     def sample_config(self):
-        return {'cloud': 'cloud name', 'task-yaml': 'path to the valid task yaml file'}
+        return {'cloud': 'cloud name', 'task-yaml': 'path to the valid task yaml file', 'rally-repo': 'http', 'rally-patch': ''}
 
     def __init__(self, config):
         from lab.with_config import read_config_from_file
 
         super(RunnerRally, self).__init__(config=config)
-        self.cloud_name = config['cloud']
-        self.task_yaml_path = config['task-yaml']
-        self.task_body = read_config_from_file(yaml_path=self.task_yaml_path, directory='rally', is_as_string=True)
+        self._cloud_name = config['cloud']
+        self._task_body = read_config_from_file(yaml_path=config['task-yaml'], directory='artifacts', is_as_string=True)
+        self._rally_repo = config['rally-repo']
+        self._rally_patch = config['rally-patch']
 
     def execute(self, clouds, servers):
         import os
 
-        cloud = filter(lambda x: x.name == self.cloud_name, clouds)
+        cloud = filter(lambda x: x.name == self._cloud_name, clouds)
         if not cloud:
-            raise RuntimeError('Cloud <{0}> is not provided by deployment phase'.format(self.cloud_name))
+            raise RuntimeError('Cloud <{0}> is not provided by deployment phase'.format(self._cloud_name))
 
         server = servers[0] if servers else cloud[0].mediator
-        open_rc_path = '{0}.openrc'.format(self.cloud_name)
+        open_rc_path = '{0}.openrc'.format(self._cloud_name)
         rally_html = 'rally-results.html'
         task_path = 'rally-task.yaml'
         venv_path = '~/venv_rally'
@@ -32,15 +33,15 @@ class RunnerRally(Runner):
 
         server.create_user(new_username='rally')
         server.put_string_as_file_in_dir(string_to_put=open_rc_body, file_name=open_rc_path)
-        server.put_string_as_file_in_dir(string_to_put=self.task_body, file_name=task_path)
+        server.put_string_as_file_in_dir(string_to_put=self._task_body, file_name=task_path)
 
         rally_installed = server.check_rally()
         if not rally_installed:
-            repo_dir = server.clone_repo(repo_url='https://git.openstack.org/openstack/rally.git', tags='0.2.0')
+            repo_dir = server.clone_repo(repo_url=self._rally_repo, patch=self._rally_patch, tags='0.2.0')
             server.check_or_install_packages(package_names='libffi-devel gmp-devel postgresql-devel wget python-virtualenv')
             server.run(command='git apply {0}'.format(patch_path), in_directory=repo_dir, warn_only=True)
             server.run(command='./install_rally.sh -y -d {0}'.format(venv_path), in_directory=repo_dir)
-            server.run(command='source {0} && {1}/bin/rally deployment create --fromenv --name {2}'.format(open_rc_path, venv_path, self.cloud_name))
+            server.run(command='source {0} && {1}/bin/rally deployment create --fromenv --name {2}'.format(open_rc_path, venv_path, self._cloud_name))
 
             rally_installed = True
 
