e5b0abd746f444ef3a07602272eeb385e908ea2f
Kirill Shileev <kshileev@gmail.com>
2016-04-05 19:38:00 +0300

    Add cloud operations Add cloud cmd runner to fabfile
    Change-Id: I41353432a33b7511c49706c1d2d042bc29db1e05

--- lab/deployers/__init__.py
+++ lab/deployers/__init__.py
@@ -9,14 +9,3 @@ class Deployer(WithConfig):
         :param list_of_servers: list of server provided during provisioning phase
         """
         pass
-
-    def verify_cloud(self, cloud, from_server):
-        from_server.run(command='neutron net-list {cloud}'.format(cloud=cloud))
-        from_server.run(command='neutron port-list {cloud}'.format(cloud=cloud))
-        from_server.run(command='neutron router-list {cloud}'.format(cloud=cloud))
-        from_server.run(command='openstack server list {cloud}'.format(cloud=cloud))
-        for service in cloud.services():
-            for url in ['publicURL', 'internalURL', 'adminURL']:
-                end_point = from_server.run(command='openstack catalog show {service} {cloud} | grep {url} | awk \'{{print $4}}\''.format(cloud=cloud, service=service, url=url))
-                cloud.add_service_end_point(service=service, url=url, end_point=end_point)
-        return cloud
