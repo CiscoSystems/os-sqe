fc0d8b3aadf1d707a6d13921067109ab8535e8dd
Kirill Shileev <kshileev@gmail.com>
2016-04-21 13:44:01 +0300

    add time filter to RunnerGetLogs
    Change-Id: I96bc03dff8bb641d5ae40126a57191a980882df3

--- lab/runners/runner_get_logs.py
+++ lab/runners/runner_get_logs.py
@@ -4,12 +4,19 @@ from lab.runners import Runner
 class RunnerGetLogs(Runner):
 
     def sample_config(self):
-        return {'cloud': 'cloud name', 'regex': 'some regexp to be used in grep'}
+        return {'cloud': 'string: cloud name',
+                'regex': 'string: some regexp to be used in grep, empty means all',
+                'minutes': 'int: 10 -> filter out all messages older then 10 minutes ago, 0 means no filter'}
 
     def __init__(self, config):
         super(RunnerGetLogs, self).__init__(config=config)
-        self._cloud_name = config['cloud']
-        self._regex = config['regex']
+        regex = config['regex']
+        minutes = int(config['minutes'])
+
+        cmd = 'sudo '
+        cmd += 'sed -n "/^$(date +%Y-%m-%d\ %H:%M --date="{min} min ago")/, /^$(date +%Y-%m-%d\ %H:%M)/p" '.format(min=minutes) + '/var/log/{log}/*.log' if minutes else 'cat /var/log/{log}/*.log '
+        cmd += '| grep -i ' + regex if regex else ''
+        self._command = cmd
 
     def execute(self, clouds, servers):
         from lab.with_config import open_artifact
@@ -17,7 +24,7 @@ class RunnerGetLogs(Runner):
         for d in ['neutron', 'nova']:
             for server in servers:
                 with open_artifact(name='{0}-{1}-errors.txt'.format(server.name(), d), mode='w') as f:
-                    f.write(server.run(command='sudo grep -i {regex} /var/log/{d}/*.log'.format(regex=self._regex, d=d), warn_only=True))
+                    f.write(server.run(command=self._command.format(log=d), warn_only=True))
 
     @staticmethod
     def store_on_our_server():
@@ -26,7 +33,7 @@ class RunnerGetLogs(Runner):
         from lab.server import Server
 
         destination_dir = '{0}-{1}'.format(lab.JENKINS_TAG, lab.REPO_TAG)
-        server = Server(ip='172.29.173.233', username='localadmin', password='ubuntu')
+        server = Server(ip='172.29.173.233', username='localadmin', password='ubuntu', lab=None, name='FileStorage')
         server.run(command='mkdir -p /var/www/logs/{0}'.format(destination_dir))
         server.put(local_path='*.log', remote_path='/var/www/logs/' + destination_dir, is_sudo=False)
         server.put(local_path='artifacts/*', remote_path='/var/www/logs/' + destination_dir, is_sudo=False)
