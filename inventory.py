bf6359235c3549fd4816fe66660740232d0bbba7
Kirill Shileev <kshileev@gmail.com>
2016-08-18 14:39:35 +0300

    ansible: added inventory.py
    Change-Id: Ic9da8d4b4faa51f1701135bfd9d443d474c45d41

---
+++ inventory.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# Get dynamic inventory for ansible
+
+
+def parse_args():
+    import argparse
+
+    parser = argparse.ArgumentParser(description='OpenStack Inventory Module')
+    parser.add_argument('--lab', action='store_true', help='Use private address for ansible host')
+    parser.add_argument('--refresh', action='store_true', help='Refresh cached information')
+    parser.add_argument('--debug', action='store_true', default=False, help='Enable debug output')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--list', action='store_true', help='List active servers')
+    group.add_argument('--host', help='List details about the specific host')
+
+    return parser.parse_args()
+
+
+def main():
+    import json
+    import os
+    from lab.laboratory import Laboratory
+
+    os.environ['DISABLE_SQE_LOG'] = 'Yes'
+    args = parse_args()
+    l = Laboratory(config_path='g7-2')
+    inventory = {x.get_id(): {'hosts': [x.get_ssh_ip()]} for x in l.get_nodes_by_class()}
+    if args.list:
+        output = json.dumps(inventory)
+    elif args.host:
+        output = l.get_nodes_by_class()[args.host]
+    else:
+        output = 'Nothing to return'
+    print(output)
+
+
+if __name__ == '__main__':
+    main()
