4bc182fdef184e0bfad30eac766b48b3a16175bc
Kirill Shileev <kshileev@gmail.com>
2016-09-06 18:37:24 +0300

    tims: added publish results matching tc and configuration
    Change-Id: If182c4317ed8c1d0ed0a22624fa52baf201c6c56

--- lab/tims.py
+++ lab/tims.py
@@ -1,74 +1,107 @@
-TEST_CASE_TEMPLATE = """
-<Tims xmlns="http://tims.cisco.com/namespace" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://tims.cisco.com/namespace http://tims.cisco.com/xsd/Tims.xsd">
-<Credential user="dratushn" token="000000525F7G007900000000006G4700"/>
-<Case>
-<Title><![CDATA[ {title} ]]></Title>
- <Description><![CDATA[{description}]]></Description>
-        <Owner>
-            <UserID>{username}</UserID>
-            <FirstName>CiscoSystems</FirstName>
-            <LastName>os-sqe</LastName>
-            <Email>{username}@cisco.com</Email>
-        </Owner>
-<WriteAccess>member</WriteAccess>
-<Created>{created}</Created>
-<Modified>{created}</Modified>
-<ProjectID xlink:href="http://tims.cisco.com/xml/{project_id}/project.svc">{project_id}</ProjectID>
-<DatabaseID xlink:href="http://tims.cisco.com/xml/NFVICLOUDINFRA/database.svc">NFVICLOUDINFRA</DatabaseID>
-<FolderID xlink:href="http://tims.cisco.com/xml/{folder_id}/entity.svc">{folder_id}</FolderID>
-</Case>
-<Timestamp>{created}</Timestamp>
-<ExecutionTime>0.00</ExecutionTime>
-</Tims>
-"""
-
-
 class Tims(object):
     FOLDERS = {'HIGH AVAILABILITY': 'Tcbr1841f', 'NEGATIVE': 'Tcbr1979f'}
     TIMS_PROJECT_ID = 'Tcbr1p'
+
+    _OPERATION_ENTITY = 'entity'
+    _OPERATION_UPDATE = 'update'
 
     def __init__(self):
         import os
 
         self._username = os.getenv('USER')
 
-    def _api_post(self, project_id, entity_or_search, data):
+    def _api_post(self, operation, body):
         import requests
 
-        response = requests.post("http://tims.cisco.com/xml/{}/{}.svc".format(project_id, entity_or_search), data=data)
+        xml_tims_wrapper = '''
+<Tims xmlns="http://tims.cisco.com/namespace" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://tims.cisco.com/namespace http://tims.cisco.com/xsd/Tims.xsd">
+<Credential user="kshileev" token="0000003933000000000D450000000000"/>
+{body}
+</Tims>
+
+'''
+        data = xml_tims_wrapper.format(body=body)
+        response = requests.post("http://tims.cisco.com/xml/{}/{}.svc".format(self.TIMS_PROJECT_ID, operation), data=data)
         if u"Error" in response.content.decode():
             raise RuntimeError(response.content)
         return response.content
-
-    def create_or_update_test_case(self, folder_id, test_name, test_description):
-        import datetime
-
-        data = TEST_CASE_TEMPLATE.format(username=self._username, created=datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S"), title=test_name, description=test_description,
-                                         folder_id=folder_id, project_id=self.TIMS_PROJECT_ID)
-        return self._api_post(project_id=self.TIMS_PROJECT_ID, entity_or_search='entity', data=data)
 
     def publish_tests_to_tims(self):
         from lab import with_config
 
         available_tc = with_config.ls_configs(directory='ha')
-        tests = sorted(filter(lambda x: 'tc-vts' in x, available_tc))
+        test_cfg_pathes = sorted(filter(lambda x: 'tc-vts' in x, available_tc))
 
-        for test in tests:
-            body = with_config.read_config_from_file(config_path=test, directory='ha', is_as_string=True)
+        test_case_template = '''
+        <Case>
+            <Title><![CDATA[ {test_name} ]]></Title>
+            <Description><![CDATA[{description}]]></Description>
+            <LogicalID>{logical_id}</LogicalID>
+            <Owner>
+                <UserID>{username}</UserID>
+                <Email>{username}@cisco.com</Email>
+            </Owner>
+            <WriteAccess>member</WriteAccess>
+            <ProjectID xlink:href="http://tims.cisco.com/xml/{project_id}/project.svc">{project_id}</ProjectID>
+            <DatabaseID xlink:href="http://tims.cisco.com/xml/NFVICLOUDINFRA/database.svc">NFVICLOUDINFRA</DatabaseID>
+            <FolderID xlink:href="http://tims.cisco.com/xml/{folder_id}/entity.svc">{folder_id}</FolderID>
+        </Case>
+        '''
+
+        body = ''
+        for test_cfg_path in test_cfg_pathes:
+            cfg_body = with_config.read_config_from_file(config_path=test_cfg_path, directory='ha', is_as_string=True)
 
             folder_name = None
-            test_name = ' '.join(test.strip('.yaml').split('-')[2:])
+            test_name = ' '.join(test_cfg_path.strip('.yaml').split('-')[2:])
 
-            for d in with_config.read_config_from_file(config_path=test, directory='ha'):
+            for d in with_config.read_config_from_file(config_path=test_cfg_path, directory='ha'):
                 folder_name = d.get('Folder', folder_name)
 
             if not folder_name:
-                raise ValueError('test {} does not specify - Folder: {}'.format(test, self.FOLDERS.keys()))
+                raise ValueError('test {} does not specify - Folder: {}'.format(test_cfg_path, self.FOLDERS.keys()))
 
             try:
                 folder_id = self.FOLDERS[folder_name]
             except KeyError:
-                raise ValueError('test {} specifies wrong Folder, possible values {}'.format(test, self.FOLDERS.keys()))
+                raise ValueError('test {} specifies wrong Folder, possible values {}'.format(test_cfg_path, self.FOLDERS.keys()))
 
-            description = 'This is the configuration actually used in testing:\n' + body + '\nuploaded from https://raw.githubusercontent.com/CiscoSystems/os-sqe/master/configs/ha/{}'.format(test)
-            self.create_or_update_test_case(folder_id=folder_id, test_name=test_name, test_description=description)
+            description = 'This is the configuration actually used in testing:\n' + cfg_body + '\nuploaded from <a href="https://raw.githubusercontent.com/CiscoSystems/os-sqe/master/configs/ha/{}">'.format(test_cfg_path)
+            body += test_case_template.format(username=self._username, test_name=test_name, logical_id=test_cfg_path, description=description, folder_id=folder_id, project_id=self.TIMS_PROJECT_ID)
+
+        self._api_post(operation=self._OPERATION_UPDATE, body=body)
+
+    def publish_result_to_tims(self, name, mercury_version, vts_version, lab, n_exceptions):
+        description = 'VTS version: {} Mercury version: {} number of exceptions {}'.format(vts_version, mercury_version, n_exceptions)
+        status = 'failed' if n_exceptions > 0 else 'passed'
+
+        result_template = '''
+        <Result>
+                <Title><![CDATA[Result for {test_name}]]></Title>
+                <Description><![CDATA[{description}]]></Description>
+                <Owner>
+                        <UserID>{username}</UserID>
+                </Owner>
+                <ListFieldValue multi-value="true">
+                    <FieldName><![CDATA[ Software Version ]]></FieldName>
+                    <Value><![CDATA[ {mercury_version} ]]></Value>
+                </ListFieldValue>
+
+                <Status>{status}</Status>
+                <CaseLookup>
+                        <TextFieldValue searchoperator="is">
+                                <FieldName>Logical ID</FieldName>
+                                <Value>{test_name}</Value>
+                        </TextFieldValue>
+                </CaseLookup>
+                <ConfigLookup>
+                        <TextFieldValue searchoperator="is">
+                                <FieldName>Logical ID</FieldName>
+                                <Value>{lab}</Value>
+                        </TextFieldValue>
+                </ConfigLookup>
+        </Result>
+        '''
+
+        body = result_template.format(username=self._username, test_name=name, description=description, mercury_version=mercury_version, status=status, lab=lab)
+        self._api_post(operation=self._OPERATION_ENTITY, body=body)
