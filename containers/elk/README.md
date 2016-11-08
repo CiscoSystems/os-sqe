9834bba6ff6fd7ee6c20515ac812c22f025606fa
Kirill Shileev <kshileev@gmail.com>
2016-11-08 20:54:53 +0300

    elk: use mercury like vmtp filtering
    Change-Id: Ib6fcf2b200bfa3675579ec5ebc0c231407c0e8c3

--- containers/elk/README.md
+++ containers/elk/README.md
@@ -1,4 +1,5 @@
 ## Building container
+* http://elk-docker.readthedocs.io/
 * Run in this directory
 ```sh  
 $ sudo docker build -t sqe/elk --no-cache --force-rm .
@@ -16,8 +17,11 @@ $  sudo sysctl -w vm.max_map_count=262144
 ```
 * Finally run 
 ```sh
-$  sudo docker run -p 7001:5601 -p 7002:9200 -p 7003:25826 --name elk sqe/elk 
+$  sudo docker run -p 7001:5601 -p 7002:9200 --name elk sqe/elk -it 
 ```
-* Your kibana will be at http://<ip>:7001
-* ES at http://<ip>:7002/_search?pretty
-* and logstash is listening at 7003
+* Your kibana will be at http://<ip>:7001 (in container at 5601)
+* ES at http://<ip>:7002/_search?pretty (in container at 9200)
+* logstash beat is listening at 7003 (in container at 5044)
+* In addition:
+* logstash lamberjack listening at 7080
+* /tmp/sqe.log is monitored 
--- tools/fabs/elk.py
+++ tools/fabs/elk.py
@@ -2,40 +2,17 @@ from fabric.api import task
 
 
 @task
-def run_elk():
-    """fab elk.run_elk \t\t\t\t Deploy elk stack as docker container."""
-    from fabric.api import sudo
-    from fabric.operations import put
-
-    image_name = 'sebp/elk'
-    container_name = 'elk'
-    remote_beats_input_conf = '/tmp/02-beats-input.conf'
-
-    get_container_id = lambda: sudo("docker ps | grep {0} | awk '{{print $1}}'".format(container_name)).stdout.strip()
-
-    sudo('docker pull {0}'.format(image_name))
-    container_id = get_container_id()
-    if not container_id:
-        sudo('docker run -p 7777:5601 -p 9999:9200 -p 7744:5044 -p 7700:5000 -it --name {0} {1}'.format(container_name, image_name), warn_only=True)
-        container_id = get_container_id()
-        container_id = get_container_id()
-
-    put('configs/elk/02-beats-input.conf', remote_beats_input_conf)
-    sudo('docker cp {0} {1}:/etc/logstash/conf.d/02-beats-input.conf'.format(remote_beats_input_conf, container_id))
-    sudo('docker exec {0} /opt/logstash/bin/plugin install logstash-filter-elapsed'.format(container_id))
-    sudo('docker exec {0} service logstash restart'.format(container_id))
-
-@task
 def json_to_es():
     """fab elk.json_to_es \t\t\t\t Push json.log to our ES and index it."""
-    from lab.nodes.lab_server import LabServer
+    from lab.server import Server
 
     with open('json.log') as l:
         with open('elk', 'w') as e:
             for line in l:
                 e.write('{ "index" : { "_index" : "sqe", "_type" : "type1"} }\n')
                 e.write(line)
-    LabServer.r_local('curl -s -XPOST 172.29.173.236:9999/_bulk --data-binary @elk')
+    server = Server(ip='localhost', username=None, password=None)
+    server.exe('curl -s -XPOST 172.29.173.236:9999/_bulk --data-binary @elk')
 
 
 @task
@@ -43,6 +20,7 @@ def kill_index(index):
     """fab elk.kill_index:index_name \t\t Kill given index in ES.
         :param index: name of index to delete
     """
-    from lab.nodes.lab_server import LabServer
+    from lab.server import Server
 
-    LabServer.r_local('curl -s -XDELETE 172.29.173.236:9999/{index}'.format(index=index))
+    server = Server(ip='localhost', username=None, password=None)
+    server.exe('curl -s -XDELETE 172.29.173.236:9999/{index}'.format(index=index))
