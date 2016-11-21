fce3246c7d83cb7ff7b3a539387d2042cacc5b4e
Kirill Shileev <kshileev@gmail.com>
2016-11-21 16:32:23 +0300

    runner: fix return code processing
    Change-Id: Ia57dddbf17db803f5c8881585a027852be5ebb0a

--- lab/base_lab.py
+++ lab/base_lab.py
@@ -62,5 +62,6 @@ class BaseLab(WithStatusMixIn):
             lab_logger.info(separator)
             lab_logger.info('Call {}.execute()...'.format(obj))
             lab_logger.info(separator)
-            self._results.append({'class': str(obj), 'spent_time': time.time() - start_time, 'status': obj.execute(self._servers_and_clouds)})
+            status = obj.execute(self._servers_and_clouds)
+            self._results.append({'class': str(obj), 'spent_time': time.time() - start_time, 'status': status})
         return self._results
