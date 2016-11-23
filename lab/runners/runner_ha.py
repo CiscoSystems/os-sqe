5ef3ce82498dd97ccc6e374eb2811615dd58ba58
Kirill Shileev <kshileev@gmail.com>
2016-11-23 19:50:30 +0300

    ha: do tims report after elk processing
    Change-Id: I31764a6040099eeae5a618f92b668353d5444fc4

--- lab/runners/runner_ha.py
+++ lab/runners/runner_ha.py
@@ -70,6 +70,9 @@ class RunnerHA(LabWorker):
         for result in results:
             exceptions.extend(result.get('exceptions', []))
 
+        elk = Elk(proxy=lab.get_director())
+        elk.filter_date_range(start=start_time)
+
         tims_report = ''
         if self._is_report_to_tims:
             t = Tims()
@@ -77,8 +80,5 @@ class RunnerHA(LabWorker):
             report_url = t.publish_result_to_tims(test_cfg_path=self._task_yaml_path, mercury_version=mercury_version, lab=lab, results=results)
             tims_report = 'and reported to {}'.format(report_url)
         self.log_to_slack(message=' {} finished with # exceptions={} {}'.format(type_of_run, len(exceptions), tims_report))
-
-        elk = Elk(proxy=lab.get_director())
-        elk.filter_date_range(start=start_time)
 
         return exceptions
