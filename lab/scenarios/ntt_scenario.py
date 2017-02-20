76567f810157f60b259f2dfe6f58e8714711607d
Kirill Shileev <kshileev@gmail.com>
2017-02-20 18:29:28 +0300

    ntt: first version of NTT scaling automation

--- lab/scenarios/ntt_scenario.py
+++ lab/scenarios/ntt_scenario.py
@@ -3,25 +3,22 @@ from lab.parallelworker import ParallelWorker
 
 class NttScenario(ParallelWorker):
     def check_arguments(self, **kwargs):
-        if 'pauses' not in kwargs or type(kwargs['pauses']) is not list:
-            raise ValueError('{}: specify pauses: [5, 4, 3, 2, 1]'.format(self))
-        self._n_repeats = len(kwargs['pauses'])
+        pass
 
     def setup_worker(self):
-        import os
+        self.get_cloud().os_image_create('csr')
+        self._build_node.r_clone_repo(repo_url='http://gitlab.cisco.com/openstack-cisco-dev/osqe-configs.git', local_repo_dir='os-sqe-tmp/osqe-configs')
+        # self._build_node.r_clone_repo(repo_url='http://gitlab.cisco.com/openstack-perf/nfvi-test.gi', local_repo_dir='os-sqe-tmp/nfvi-test')
+        # self._build_node.r_clone_repo(repo_url='http://gitlab.cisco.com/openstack-perf/testbed', local_repo_dir='os-sqe-tmp/testbed')
 
-        for file_name in os.listdir('lab/scenarios/csr_bash'):
-            with open('lab/scenarios/csr_bash/' + file_name, 'r') as f:
-                body = f.read()
-            self._build_node.r_put_string_as_file_in_dir(string_to_put=body, file_name=file_name, in_directory='ntt_scenario')
-        self._build_node.exe('chmod +x *', in_directory='ntt_scenario')
-        self._build_node.r_get_remote_file(url='http://172.29.173.233/csr/csr1000v-universalk9.03.16.00.S.155-3.S-ext.qcow2', to_directory='ntt_scenario')
-        self._build_node.exe('mkdir -p ntt_scenario/cfg')
+        self._build_node.exe('docker pull cloud-docker.cisco.com/nfvbench')
+        self._build_node.exe('yum install kernel-devel kernel-headers -y')
+        self._build_node.exe('docker run --rm --privileged -v /lib/modules/$(uname -r):/lib/modules/$(uname -r) -v /usr/src/kernels/$(uname -r):/usr/src/kernels/$(uname -r) cloud-docker.cisco.com/nfvbench nfvbench -h')
+        self._build_node.exe('docker run --rm --privileged -v $PWD:/tmp/nfvbench cloud-docker.cisco.com/nfvbench nfvbench_config', in_directory='os-sqe-tmp')
 
     def loop_worker(self):
-        pause = self._kwargs['pauses'][self._loop_counter]
-        answers = self._build_node.exe('source $HOME/openstack-configs/openrc && unalias cp && . csr_scenario.sh -l {} -d {}'.format(10, pause), in_directory='ntt_scenario', is_warn_only=True)
+        answers = self._build_node.exe('./nfvbench.sh -c os-sqe-tmp/osqe-configs/nfvbench/nfvbench_config.yaml -g trex-local --rate 1500pps --json results.json', in_directory='os-sqe-tmp', is_warn_only=True)
         return answers
 
     def teardown_worker(self):
-        self._build_node.exe('rm -rf ntt_scenario /tmp/tmp.*')
+        self._build_node.exe('rm -rf os-sqe-tmp')
