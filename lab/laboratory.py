5b315219491557d4646b50eeba0cce4e8298dab0
Kirill Shileev <kshileev@gmail.com>
2017-03-13 15:05:21 +0300

    lab: exclude OOB, PXE and TOR from collect info

--- lab/laboratory.py
+++ lab/laboratory.py
@@ -256,11 +256,15 @@ class Laboratory(WithMercuryMixIn, WithOspd7, WithLogMixIn, WithConfig):
             node.r_border_leaf()
 
     def r_collect_information(self, regex, comment):
+        from lab.nodes.tor import Oob, Tor, Pxe
+
         logs = 'LOGS:\n'
         configs = '\n\nCONFIGS:\n'
         cloud_version, vts_version = self.r_get_version()
         self.log_to_artifact(name='{}-version.txt'.format(self), body='Version={}\n{}'.format(cloud_version, vts_version))
         for node in self.get_nodes_by_class():
+            if type(node) in [Oob, Tor, Pxe]:
+                continue
             if hasattr(node, 'r_collect_logs'):
                 logs += node.r_collect_logs(regex=regex)
             if hasattr(node, 'r_collect_config'):
