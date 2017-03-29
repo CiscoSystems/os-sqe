06dc63115238ae8102ab299635bd66a2fceec759
Kirill Shileev <kshileev@gmail.com>
2017-03-29 19:47:36 +0300

    ntt: fixes to retrieve info from nfvbench json

--- lab/scenarios/ntt_scenario.py
+++ lab/scenarios/ntt_scenario.py
@@ -73,7 +73,7 @@ class NttScenario(ParallelWorker):
             self.csr_run()
 
         if self._what_to_run in ['nfvbench', 'both']:
-            self.single_nfvbench_run(parameters='--rate 1.3Gbps --service-chain {} --service-chain-count {} --flow-count {}'.format(self._chain_type, self._chain_count, self._flow_count))
+            self.single_nfvbench_run(parameters='--rate ndr_pdr --service-chain {} --service-chain-count {} --flow-count {}'.format(self._chain_type, self._chain_count, self._flow_count))
 
     def csr_run(self):
 
@@ -94,7 +94,7 @@ class NttScenario(ParallelWorker):
         container_info = ''
         cmd = self._nfvbench_cmd + ' {} {}'.format(container_info, parameters, '--no-cleanup' if self.is_noclean else '')
         ans = self.get_mgmt().exe(cmd, in_directory=self._tmp_dir, is_warn_only=True)
-        with self.get_lab().open_artifact('nfvbench_output.txt', 'w') as f:
+        with self.get_lab().open_artifact('nfvbench_output_{}.txt'.format(parameters.replace(' ', '_')), 'w') as f:
             f.write(cmd + '\n')
             f.write(ans)
 
@@ -102,21 +102,23 @@ class NttScenario(ParallelWorker):
             raise RuntimeError(ans.split('ERROR')[1][:200])
         else:
             res_json_body = self.get_mgmt().r_get_file_from_dir(file_name='results.json', in_directory=self._tmp_dir)
-            with self.get_lab().open_artifact('{}-{}.json'.format(self._chain_type, self._chain_count, self._flow_count), 'w') as f:
-                f.write(res_json_body)
-            self.retrieve_values_from_nfvbench_json(parameters=parameters, res_json_body=res_json_body)
+            self.process_nfvbench_json(parameters=parameters, res_json_body=res_json_body)
 
-    def retrieve_values_from_nfvbench_json(self, parameters, res_json_body):
+    def process_nfvbench_json(self, parameters, res_json_body):
         import json
 
-        res_json = json.loads(res_json_body)
+        j = json.loads(res_json_body)
+
+        with self.get_lab().open_artifact('{}-{}-{}-{}.json'.format(j['openstack_spec']['vswitch'], j['config']['service_chain'], j['config']['service_chain_count'], j['config']['flow_count']), 'w') as f:
+            f.write(res_json_body)
+
         res = []
-        for mtu, di in res_json['benchmarks']['network']['service_chain']['PVP']['result'][0]['result'].items():
+        for mtu, di in j['benchmarks']['network']['service_chain'][j['config']['service_chain']]['result']['result'].items():
             if 'ndr' in di:
                 for t in ['ndr', 'pdr']:
                     la_min, la_avg, la_max = di[t]['stats']['overall']['min_delay_usec'], di[t]['stats']['overall']['avg_delay_usec'], di[t]['stats']['overall']['max_delay_usec']
                     gbps = di[t]['rate_bps'] / 1e9
-                    drop_thr = di[t]['stats']['overall']['drop_rate_percent']
+                    drop_thr = di[t]['stats']['overall']['drop_percentage']
                     res.append('MTU={} {}({:.4f}) rate={:.4f} Gbps latency={:.1f} {:.1f} {:.1f} usec'.format(mtu, t, drop_thr, gbps, la_min, la_avg, la_max))
             else:
                 res.append('MTU={} RT={}'.format(mtu, di['stats']['overall']['rx']['pkt_bit_rate'] + di['stats']['overall']['tx']['pkt_bit_rate']))
