5f4685e2d1a0d4bd9ecd796ae27953128e9c951c
Kirill Shileev <kshileev@gmail.com>
2017-03-29 16:40:38 +0300

    nfvbench: refactor bash starter to create start_nfv fro debugging

--- lab/scenarios/nfvbench.sh
+++ lab/scenarios/nfvbench.sh
@@ -1,19 +1,10 @@
 NFVBENCH_CONTAINER="cloud-docker.cisco.com/nfvbench"
-
+MERCURY_VERSION=$(cat /etc/cisco-mercury-release)
 KERNEL=$(uname -r)
+V_KERNEL="-v /lib/modules/${KERNEL}:/lib/modules/${KERNEL} -v /usr/src/kernels/${KERNEL}:/usr/src/kernels/${KERNEL}"
+V_PARAMS="-v \${PWD}:/tmp/nfvbench -v /etc/hosts:/etc/hosts -v ${HOME}/.ssh:/root/.ssh -v /dev:/dev ${V_KERNEL} -v /root/openstack-configs:/tmp/nfvbench/openstack"
+ALL_PARAMS="--privileged --net host ${V_PARAMS} --name nfvbench_${MERCURY_VERSION} ${NFVBENCH_CONTAINER}"
 
-NFVBENCH_COMMAND="docker run \
-    --rm \
-    --privileged \
-    --net host \
-    -it \
-    -v ${PWD}:/tmp/nfvbench \
-    -v /etc/hosts:/etc/hosts \
-    -v ${HOME}/.ssh:/root/.ssh \
-    -v /dev:/dev \
-    -v /lib/modules/${KERNEL}:/lib/modules/${KERNEL} \
-    -v /usr/src/kernels/${KERNEL}:/usr/src/kernels/${KERNEL} \
-    -v /root/openstack-configs:/tmp/nfvbench/openstack \
-    ${NFVBENCH_CONTAINER} nfvbench"
+grep -q -F "alias start_nfv=" /root/.bashrc || echo alias start_nfv=\'docker run -d ${ALL_PARAMS}\' >> /root/.bashrc
 
-$NFVBENCH_COMMAND -c /tmp/nfvbench/nfvbench_config.yaml --json /tmp/nfvbench/results.json $*
+docker run --rm -it ${ALL_PARAMS} nfvbench -c /tmp/nfvbench/nfvbench_config.yaml --json /tmp/nfvbench/results.json $*
