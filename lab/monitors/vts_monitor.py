fee1ca04b927c5a856d985a5df636a178f3ee796
kshileev <kshileev@cisco.com>
2018-03-15 14:42:28 +0300

    vtc mon: better logging

--- lab/monitors/vts_monitor.py
+++ lab/monitors/vts_monitor.py
@@ -11,5 +11,6 @@ class VtsMonitor(TestCaseWorker):
 
     def loop_worker(self):
         nets, srvs = self.pod.vtc.api_openstack()
-        self.pod.vtc.r_vtc_crm_mon('monitor'),
-        self.log('n_nets={}'.format(len(nets)))
+        ha = self.pod.vtc.r_vtc_crm_mon()
+
+        self.log('n_nets={} master={} slave={} online={} offline={} when=in_monitor'.format(len(nets), ha['master'], ha['slave'], ha['online'], ha['offline']))
